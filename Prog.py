64edf6a222bf83ab0c088b31aa5a59ecc8c1074c
Ckao123456789 <43617720+KaoCameron@users.noreply.github.com>
2018-12-05 12:29:36 -0500

    Update Prog.py

--- Prog.py
+++ Prog.py
@@ -1,5 +1,4 @@
-def draw():
-    global x, word, y
+ global x, word, y
     background(255)
     if word <= 100:
         word += 0.2
@@ -14,12 +13,7 @@ def draw():
     textSize(40)
     text(int(word), 360, 200)
     text("%", y, 200)
-    noFill()# computer-science-enviromnet
-thingy
-Class collaboration on hangman.
- Python  50 Updated on Oct 5
-￼Star
-
+    noFill()
     rect(115, 200,500,75)
     fill(1)
     rect(115, 200, x, 75)
