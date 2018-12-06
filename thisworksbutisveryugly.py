e8eae96d82273c0f905e871c81b05cc07a8951e8
FTadros <43042263+FTadros@users.noreply.github.com>
2018-12-06 16:52:45 -0500

    Update thisworksbutisveryugly.py

--- thisworksbutisveryugly.py
+++ thisworksbutisveryugly.py
@@ -20,12 +20,15 @@ def keyTyped():
      global weight,r 
      weight += key
      r = random(0, int(weight))
-     print int(r)
+     print ceil(r)
 
 def draw():
     global weight,word,x,y
-    background('#231123')
-    noStroke ()
+    background('#9286C9')
+    textAlign(LEFT, TOP)
+    textSize(50)
+    text(weight,0,0)
+    stroke(5)
     fill('#FFEFFF')
     if key == ENTER:
         if word <= 100:
@@ -41,41 +44,41 @@ def draw():
         
         textAlign(CENTER, BOTTOM)
         textSize(40)
-        text(int(word), progress_x+500/2, progress_y)
+        text(ceil(word), progress_x+500/2, progress_y)
         text("%", y, 200)
-        fill(255)
+        fill('#E4E1F7')
         rect(progress_x, progress_y, 500, 75)
-        fill(1)
+        fill('#6C5BC1')
         rect(progress_x, progress_y, x, 75)
         if x< 500:
             x += 1
         elif x >= 500:
             global t,r,weight
             weight = int(weight)  
-            t -= .1
+            t -= .07
             fill('#FFEFFF')
             textAlign(CENTER, BOTTOM)
-            text('Complete! continuing in %s'% (int(t)), 350, 400)
+            text('Complete! continuing in %s'% (ceil(t)), 350, 400)
             if t <= 0 and r< weight/2 :
-                background('#231123')
+                background('#9286C9')
                 textSize(32) 
                 text('The program has found:',350,100)
                 textSize(16)
-                text("%s kg of valubale waste(gold, platinum etc)" % (int(r)),350,150)
-                text("The rest, %s, was safe garbage" % int((weight-r)),350,200)
+                text("%s kg of valubale waste(gold, platinum etc)" % (ceil(r)),350,150)
+                text("The rest, %s, was safe garbage" % (weight-r),350,200)
             if r >= weight/2:
-                background('#231123')
+                background('#9286C9')
                 textSize(32)
                 text('The program has found:',350,100)
                 textSize(16)
-                text("%s kg of valubale waste (gold, platinum etc)" % (int(r)) ,350,200)
-                text('Since there is still a decent amount (%s) of ' % int((weight-r)), 350, 250)
+                text("%s kg of valubale waste (gold, platinum etc)" % (ceil(r)) ,350,200)
+                text('Since there is still a decent amount (%s) kg of ' % (weight-r), 350, 250)
                 text('precious metals inside your garabge, ',350,300)
                 text('please send it to an extraction plant',350,340)
 
     else:
+        stroke(5)
         textSize(32) 
         textAlign(CENTER, CENTER)
         text("Insert Weight Of Waste, in Kg", text_x, text_y)
         text(weight, text_x, text_y+100)
-
